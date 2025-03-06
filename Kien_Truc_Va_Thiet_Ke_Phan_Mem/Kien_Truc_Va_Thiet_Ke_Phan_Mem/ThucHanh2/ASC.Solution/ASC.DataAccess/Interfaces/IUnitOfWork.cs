9fc0ee10b8296efd9ff51a5566fc434413d54123
NguyenVietDuong <duongnguyen15042004@gmail.com>
2025-03-06 19:24:46 +0700

    Add project files.

---
+++ Kien_Truc_Va_Thiet_Ke_Phan_Mem/Kien_Truc_Va_Thiet_Ke_Phan_Mem/ThucHanh2/ASC.Solution/ASC.DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using ASC.Model.BaseTypes;
+
+namespace ASC.DataAccess.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : BaseEntity;
+
+        public int CommitTransaction();
+    }
+}
